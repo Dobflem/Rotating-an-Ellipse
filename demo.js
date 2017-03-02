af818a516a0bb55b8982ac8a1a202440ac03ce4a
Russell Hickey <RHickeyJnr@gmail.com>
2017-03-02 22:12:38 +0000

    Fix indentation

--- demo.js
+++ demo.js
@@ -5,212 +5,217 @@ if (typeof(Number.prototype.toRad) === "undefined") {
 }
 
 var M = (function() {
-	this.init = function() {
-		this.center = { x: 200, y: 200 };
-		this.circle = null;
-		this.height = 40;
-		this.index = 0;
-		this.interval_speed = 100;
-		this.paper = null;
-		this.segments = 4;
-		this.segment_angle = 360 / this.segments;
-		this.state = 0;
-		this.width = 80;
-	};
 
-	this.getCenterPoint = function() {
-        return this.center;
-    };
+  this.init = function() {
+    this.center = { x: 200, y: 200 };
+    this.circle = null;
+    this.height = 40;
+    this.index = 0;
+    this.interval_speed = 100;
+    this.paper = null;
+    this.segments = 4;
+    this.segment_angle = 360 / this.segments;
+    this.state = 0;
+    this.width = 80;
+  };
 
-	this.getCircle = function() {
-		return this.circle;
-	};
+  this.getCenterPoint = function() {
+    return this.center;
+  };
 
-	this.getHeight = function() {
-		return this.height;
-	};
+  this.getCircle = function() {
+    return this.circle;
+  };
 
-	this.getIndex = function() {
-		return this.index;
-	};
+  this.getHeight = function() {
+    return this.height;
+  };
 
-	this.getIntervalSpeed = function() {
-		return this.interval_speed;
-	};
+  this.getIndex = function() {
+    return this.index;
+  };
 
-	this.getPaper = function() {
-		return this.paper;
-	};
+  this.getIntervalSpeed = function() {
+    return this.interval_speed;
+  };
 
-	this.getSegmentAngle = function() {
-		return this.segment_angle;
-	};
+  this.getPaper = function() {
+    return this.paper;
+  };
 
-	this.getSegments = function() {
-		return this.segments;
-	};
+  this.getSegmentAngle = function() {
+    return this.segment_angle;
+  };
 
-	this.getState = function() {
-		return this.state;
-	};
+  this.getSegments = function() {
+    return this.segments;
+  };
 
-	this.getWidth = function() {
-		return this.width;
-	};
+  this.getState = function() {
+    return this.state;
+  };
 
-	this.incrementIndex = function(amount) {
-		this.index += amount;
-	};
+  this.getWidth = function() {
+    return this.width;
+  };
 
-	this.setCircle = function(circle) {
-		this.circle = circle;
-	};
+  this.incrementIndex = function(amount) {
+    this.index += amount;
+  };
 
-	this.setPaper = function(paper) {
-		this.paper = paper;
-	};
+  this.setCircle = function(circle) {
+    this.circle = circle;
+  };
 
-	this.setSegments = function(amount) {
-		this.segments = amount;
-		this.segment_angle = 360 / this.segments;
-		if (amount < 100) {
-			this.interval_speed = 100;
-        } else if (amount < 500) {
-			this.interval_speed = 60;
-		} else {
-			this.interval_speed = 30;
-		}
-	};
+  this.setPaper = function(paper) {
+    this.paper = paper;
+  };
 
-	this.setState = function(state) {
-		this.state = state;
-	};
+  this.setSegments = function(amount) {
+    this.segments = amount;
+    this.segment_angle = 360 / this.segments;
+    if (amount < 100) {
+      this.interval_speed = 100;
+    } else if (amount < 500) {
+      this.interval_speed = 60;
+    } else {
+      this.interval_speed = 30;
+    }
+  };
 
-	return {
-		getCenterPoint: this.getCenterPoint,
-		getCircle: this.getCircle,
-		getHeight: this.getHeight,
-		getIndex: this.getIndex,
-		getIntervalSpeed: this.getIntervalSpeed,
-		getPaper: this.getPaper,
-		getSegmentAngle: this.getSegmentAngle,
-		getSegments: this.getSegments,
-		getState: this.getState,
-		getWidth: this.getWidth,
-		incrementIndex: this.incrementIndex,
-		init: this.init,
-		setCircle: this.setCircle,
-		setPaper: this.setPaper,
-		setSegments: this.setSegments,
-		setState: this.setState,
-		STOPPED: 0,
-		RUNNING: 1
-	};
+  this.setState = function(state) {
+    this.state = state;
+  };
+
+  return {
+    getCenterPoint: this.getCenterPoint,
+    getCircle: this.getCircle,
+    getHeight: this.getHeight,
+    getIndex: this.getIndex,
+    getIntervalSpeed: this.getIntervalSpeed,
+    getPaper: this.getPaper,
+    getSegmentAngle: this.getSegmentAngle,
+    getSegments: this.getSegments,
+    getState: this.getState,
+    getWidth: this.getWidth,
+    incrementIndex: this.incrementIndex,
+    init: this.init,
+    setCircle: this.setCircle,
+    setPaper: this.setPaper,
+    setSegments: this.setSegments,
+    setState: this.setState,
+    STOPPED: 0,
+    RUNNING: 1
+  };
+
 })();
 
 var V = (function() {
-	this.initCircle = function(point) {
-		var paper = M.getPaper();
-		M.setCircle(paper.ellipse(point.x, point.y, 10, 10));
-	};
 
-	this.initEllipse = function() {
-		var paper = M.getPaper();
-		var center = M.getCenterPoint();
-		var width = M.getWidth();
-		var height = M.getHeight();
-		paper.ellipse(center.x, center.y, width, height);
-	};
+  this.initCircle = function(point) {
+    var paper = M.getPaper();
+    M.setCircle(paper.ellipse(point.x, point.y, 10, 10));
+  };
 
-	this.setButtonText = function(txt) {
-		$("#btnStart").html(txt);
-	};
+  this.initEllipse = function() {
+    var paper = M.getPaper();
+    var center = M.getCenterPoint();
+    var width = M.getWidth();
+    var height = M.getHeight();
+    paper.ellipse(center.x, center.y, width, height);
+  };
 
-	this.updateCirclePosition = function(point) {
-		var circle = M.getCircle();
-		circle.attr({
-		  cx: point.x,
-		  cy: point.y
-		});
-	};
+  this.setButtonText = function(txt) {
+    $("#btnStart").html(txt);
+  };
 
-	return {
-		initCircle: this.initCircle,
-		initEllipse: this.initEllipse,
-		setButtonText: this.setButtonText,
-		updateCirclePosition: this.updateCirclePosition
-	};
+  this.updateCirclePosition = function(point) {
+    var circle = M.getCircle();
+    circle.attr({
+      cx: point.x,
+      cy: point.y
+    });
+  };
+
+  return {
+    initCircle: this.initCircle,
+    initEllipse: this.initEllipse,
+    setButtonText: this.setButtonText,
+    updateCirclePosition: this.updateCirclePosition
+  };
+
 })();
 
 var C = (function() {
 
-	this.init = function() {
-		M.init();
-		M.setPaper(new Raphael("canvas", "100%", "100%"));
-		V.initEllipse();
-		V.initCircle(getXY());
-		$("#inputSegments").val(4);
-	};
+  this.init = function() {
+    M.init();
+    M.setPaper(new Raphael("canvas", "100%", "100%"));
+    V.initEllipse();
+    V.initCircle(getXY());
+    $("#inputSegments").val(4);
+  };
 
-	this.getXY = function() {
-		var index = M.getIndex();
-		var segment_angle = M.getSegmentAngle();
-		var segments = M.getSegments();
-		var angle = segment_angle * (index % segments);
-		var width = M.getWidth();
-		var height = M.getHeight();
-		var center = M.getCenterPoint();
+  this.getXY = function() {
+    var index = M.getIndex();
+    var segment_angle = M.getSegmentAngle();
+    var segments = M.getSegments();
+    var angle = segment_angle * (index % segments);
+    var width = M.getWidth();
+    var height = M.getHeight();
+    var center = M.getCenterPoint();
 
-		return {
-			x: (center.x + (width  * Math.cos(angle.toRad()))),
-		    y: (center.y + (height * Math.sin(angle.toRad())))
-		}
-	};
+    return {
+      x: (center.x + (width  * Math.cos(angle.toRad()))),
+      y: (center.y + (height * Math.sin(angle.toRad())))
+    }
+  };
 
-	this.start = function() {
-		console.log(M.getIntervalSpeed());
-		this.interval = setInterval(function() {
-			this.updateCirclePosition();
-		}, M.getIntervalSpeed());
-		M.setState(M.RUNNING);
-		V.setButtonText("Stop");
-	};
+  this.start = function() {
+    this.interval = setInterval(function() {
+      this.updateCirclePosition();
+    }, M.getIntervalSpeed());
+    M.setState(M.RUNNING);
+    V.setButtonText("Stop");
+  };
 
-	this.stop = function() {
-		clearInterval(this.interval);
-		M.setState(M.STOPPED);
-		V.setButtonText("Start");
-	};
+  this.stop = function() {
+    clearInterval(this.interval);
+    M.setState(M.STOPPED);
+    V.setButtonText("Start");
+  };
 
-	this.updateCirclePosition = function() {
-		M.incrementIndex(1);
-		V.updateCirclePosition(this.getXY(M.getIndex()));
-	};
+  this.updateCirclePosition = function() {
+    M.incrementIndex(1);
+    V.updateCirclePosition(this.getXY(M.getIndex()));
+  };
 
-	return {
-		init: this.init,
-		start: this.start,
-		stop: this.stop
-	};
+  return {
+    init: this.init,
+    start: this.start,
+    stop: this.stop
+  };
+
 })();
 
 // Run the script when the screen loads
 window.onload = function() {
 
-	$("#btnStart").on('click', function() {
-		if (M.getState() === M.RUNNING) {
-			C.stop();
-		} else {
-			C.start();
-		}
-	});
+  $("#btnStart").on('click', function() {
+    if (M.getState() === M.RUNNING) {
+      C.stop();
+    } else {
+      C.start();
+    }
+  });
 
-	$("#inputSegments").change(function() {
-		M.setSegments(parseInt($(this).val()));
-		C.stop();
-		C.start();
-	});
+  $("#inputSegments").change(function() {
+    M.setSegments(parseInt($(this).val()));
+    C.stop();
+    C.start();
+  });
 
-	// Create our paper
-	C.init();
+  // Create our paper
+  C.init();
+
 }
